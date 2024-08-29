44db1ec0148fa711adfe04e27a7f13f6839d5860
ashark <asharkhan1258@gmail.com>
2024-08-30 04:30:13 +0500

    routes updating

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // server.js
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -14,18 +14,17 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('MongoDB connected'))
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 // Define Routes
-app.use('/', async (req, res) => {
-  res.send('Spectrum Api Running...');
-});
-app.use('/api/survey', require('./routes/surveyRoutes'));
+
+app.use("/api/survey", require("./routes/surveyRoutes"));
 
 // Start the server
 app.listen(PORT, () => {
